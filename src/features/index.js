cefdc38f0f1e549a8b09b2f919927ee5083e5f62
SERDUN <dmitriiserdun@gmail.com>
2025-06-16 00:35:19 +0300

    refactor: remove unused feature package for codebase cleanup

--- src/features/index.js
+++ src/features/index.js
@@ -1,5 +1,3 @@
-export * from './habit-analytics/habit-analytics.controller.js';
-export * from './habit-analytics/habit-analytics.services.js';
 export * from './habit-management/habit_manager.controller.js';
 export * from './habit-management/habit_manager.services.js';
 export * from './habit-tracking/habit_tracking.controller.js';

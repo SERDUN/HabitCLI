266127c7aa75d59cae0d4f860894a7100135eba5
SERDUN <dmitriiserdun@gmail.com>
2025-06-16 08:49:59 +0300

    refactor: remove outdated renderer implementation

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,5 +3,4 @@ export * from './command.router.js';
 export * from './controller.js';
 export * from './identity.js';
 export * from './input.js';
-export * from './pretty.renderer.js';
 export * from './time_range.js';
